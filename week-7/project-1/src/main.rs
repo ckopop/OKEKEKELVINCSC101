abae9ac6670d0b519f709183a2e3f1a221038a1e
ckopop <chigozie.okeke@pau.edu.ng>
2024-12-08 12:51:50 -0800

    (week-8)

--- week-7/project-1/src/main.rs
+++ week-7/project-1/src/main.rs
@@ -2,12 +2,12 @@ use std::io;
 
  fn main() {
     
-    println!("Area of trapezium is {},get_Area of a Area_of_trapezium");
+    println!("Area of trapezium");
     println!("Area of a rhombus");
     println!("Area of parallelogram ");
     println!("Area of cube");
     println!("volume of a cylinder");
 }
 fn Area_of_trapezium(){
-    let(height,base1,base2);=get_
+    let(height,base1,base2);= 
 }
